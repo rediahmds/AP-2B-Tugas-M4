62cf023a62e1d4a5495476dce63842d14133bdac
Redi Ahmad <redi.ngoding@gmail.com>
2022-04-27 11:17:41 +0700

    Mengerjakan No 3.cpp

--- No 3.cpp
+++ No 3.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
-int banyakData, n, bilangan;
-double rerata;
 
 int main () {
+    float banyakData, nilai, totalNilai, rerata;
+    // Input banyaknya data
     cout<<"Masukkan banyak data: ";
     cin>>banyakData;
 
-    for (n = 1; n<=banyakData; n++) {
+    // Pendefinisian perulangan dimana n sebagai variabel iterator
+    for (int n = 1; n<=banyakData; n++) {
         cout<<"Masukkan bilangan ke-"<<n<<": ";
-        cin>>bilangan;
+        cin>>nilai;
+
+        // Assignment untuk menghitung total nilai segera setelah nilai ke-n di-input
+        totalNilai = totalNilai + nilai;
     }
 
+    cout<<"Jumlahnya = "<<totalNilai<<endl;
+    rerata = totalNilai / banyakData;
+    cout<<"Rerata = "<<rerata<<endl;
 
 }
